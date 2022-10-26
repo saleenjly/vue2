542cb339cc66d633bd95cd3f208eb59adc77ddd5
saleenji <saleenji0426@gmail.com>
2022-10-26 13:34:12 +0800

    10.26

---
+++ 12-前端模块化/02-ES6的模块化实现/aaa.js
@@ -0,0 +1,16 @@
+let name = '小明'
+let age = 18
+let flag = true
+
+function sum(num1, num2) {
+  return num1 + num2
+}
+
+if (flag) {
+  console.log(sum(20, 30))
+}
+
+export {
+  flag,
+  sum
+}
---
+++ 12-前端模块化/02-ES6的模块化实现/mmm.js
@@ -0,0 +1,6 @@
+import { flag, sum } from "./aaa.js"
+
+if (flag) {
+  console.log('小明是天才')
+  console.log(sum(20,30))
+}
